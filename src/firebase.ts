655a4e9d661d9ef35e1708500370ba1482b6188b
Aditya1942 <parmaraditya1942@gmail.com>
2021-12-30 19:38:23 +0530

    firebase init

---
+++ src/firebase.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyDMk4nExB5885CgxxT_lHNKT442nCdfXYw',
+  authDomain: 'my-projects-c6f17.firebaseapp.com',
+  projectId: 'my-projects-c6f17',
+  storageBucket: 'my-projects-c6f17.appspot.com',
+  messagingSenderId: '150792729881',
+  appId: '1:150792729881:web:f9edda11825798ee150188',
+  measurementId: 'G-G30V8YSXZ9',
+};
